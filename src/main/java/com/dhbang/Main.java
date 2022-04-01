7e41f1ce1941ca6a34b13854988bdc021a766de1
BangDDong <gusehdqkd@naver.com>
2022-04-01 18:27:28 +0900

    Delete - No user code
    * 쓰지 않는 코드 삭제

--- src/main/java/com/dhbang/Main.java
+++ src/main/java/com/dhbang/Main.java
@@ -4,8 +4,6 @@ import main.java.com.dhbang.view.MainForm;
 
 public class Main {
 
-    private static final String FILE_PATH = System.getProperty("user.dir") + "\\src\\main\\log\\input.txt";
-
     public static void main(String[] args) {
         new MainForm();
     }
